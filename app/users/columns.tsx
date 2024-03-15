35061bc19c0bb0200a4e8e3d100c31beaf552aea
msm <myatsumon@ucsy.edu.mm>
2024-03-15 07:24:36 +0630

    feat: create user

--- app/users/columns.tsx
+++ app/users/columns.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
 export type User = {
   id: number;
@@ -39,20 +32,13 @@ export const columns: ColumnDef<User>[] = [
   },
   {
     accessorKey: "actions",
-    header: 'Actions',
+    header: "Actions",
     cell: (row) => (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem>Edit</DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div>
+        <Link href={`/users/${row.getValue("id")}/edit`} className="underline">
+          Edit
+        </Link>
+      </div>
     ),
   },
 ];
--- app/users/create/page.tsx
+++ app/users/create/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
-import { useFieldArray, useForm } from "react-hook-form";
-import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormControl,
@@ -21,11 +19,39 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
 
-const profileFormSchema = z.object({
-  username: z
+export interface UserFormValues {
+  name: string;
+  email: string;
+  role: string;
+  permissions: string[];
+}
+
+const permissions = [
+  {
+    label: "READ",
+    value: "read",
+  },
+  {
+    label: "WRITE",
+    value: "write",
+  },
+  {
+    label: "UPDATE",
+    value: "update",
+  },
+  {
+    label: "DELETE",
+    value: "delete",
+  },
+];
+
+const userFormSchema = z.object({
+  name: z
     .string()
     .min(2, {
       message: "Username must be at least 2 characters.",
@@ -33,60 +59,26 @@ const profileFormSchema = z.object({
     .max(30, {
       message: "Username must not be longer than 30 characters.",
     }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
-    })
-    .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
-    )
-    .optional(),
+  email: z.string().email("Please enter valid email address"),
+  role: z.string({ required_error: "Please select a role" }),
+  permissions: z.array(z.string()).optional(),
 });
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
-  urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
-  ],
-};
-
 const CreateUser = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-  };
-
-  const form = useForm<ProfileFormValues>({
-    //    resolver: zodResolver(profileFormSchema),
-    defaultValues,
+  const form = useForm<UserFormValues>({
+    resolver: zodResolver(userFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      role: "user",
+      permissions: ["read"],
+    },
     mode: "onChange",
   });
 
-  const { fields, append } = useFieldArray({
-    name: "urls",
-    control: form.control,
-  });
-
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
+  function onSubmit(data: UserFormValues) {
+    console.log(data);
+    toast("form submitted");
   }
 
   return (
@@ -94,16 +86,16 @@ const CreateUser = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Enter Name" {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name. It can be your real name or a
-                pseudonym. You can only change this once every 30 days.
+                nickname. You can only change this once every 30 days.
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -115,21 +107,11 @@ const CreateUser = () => {
           render={({ field }) => (
             <FormItem>
               <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="m@example.com">m@example.com</SelectItem>
-                  <SelectItem value="m@google.com">m@google.com</SelectItem>
-                  <SelectItem value="m@support.com">m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
+              <FormControl>
+                <Input placeholder="Enter Email" {...field} />
+              </FormControl>
               <FormDescription>
-                You can manage verified email addresses in your{" "}
-                <Link href="/examples/forms">email settings</Link>.
+                You can manage verified email addresses in your email settings.
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -137,58 +119,58 @@ const CreateUser = () => {
         />
         <FormField
           control={form.control}
-          name="bio"
+          name="role"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Bio</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                You can <span>@mention</span> other users and organizations to
-                link to them.
-              </FormDescription>
+              <FormLabel>Role</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue="user">
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="manager">Manager</SelectItem>
+                  <SelectItem value="user">User</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>Select a role</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div>
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`urls.${index}.value`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className={cn(index !== 0 && "sr-only")}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && "sr-only")}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          ))}
-          <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="mt-2"
-            onClick={() => append({ value: "" })}
-          >
-            Add URL
-          </Button>
-        </div>
-        <Button type="submit">Update profile</Button>
+        <FormField
+          control={form.control}
+          name="permissions"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="flex space-x-2">
+                  {permissions.map((per) => (
+                    <div key={per.label} className="flex space-x-1">
+                      <Checkbox
+                        checked={field.value?.includes(per.value)}
+                        onCheckedChange={(checked) => {
+                          return checked
+                            ? field.onChange([...field.value, per.value])
+                            : field.value?.filter((val) => val !== per.value);
+                        }}
+                      />
+                      <FormLabel>{per.label}</FormLabel>
+                    </div>
+                  ))}
+                </div>
+              </FormControl>
+              <FormDescription>
+                Select the permissions for user.
+              </FormDescription>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
       </form>
     </Form>
   );
